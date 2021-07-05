b2ed8e0a0205efd97b7c1fcd7ebaccd5da348086
zhoucan <534523978@qq.com>
2021-07-05 16:00:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app{
+  height: 100vh;
+  background:url('./static/image/bg.png') no-repeat center center ,linear-gradient(154deg,#0779ff,#1fdfff);
+  background-size: cover;
+
+}
+
+</style>
